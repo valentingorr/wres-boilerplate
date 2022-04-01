0a7892cf30559a332e56af4911f56f68d1fe3a58
Valentin Gorrin <87900039+valentingorr@users.noreply.github.com>
2022-04-01 17:25:18 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,37 +10,17 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: (m) => { return /\.(js|jsx)$/.test(m) },
-				exclude: (m) => { return /node_modules/.test(m) },
-				use: {
-				loader: "babel-loader",
-				options: {
-					presets: [
-						"@babel/preset-env",
-						"@babel/preset-react"
-					]
-				}
-				}
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				use: { loader: 'babel-loader'}
 			},
-				{
-				test: (m) => { return /\.css$/.test(m) },
-				exclude: (m) => { return /node_modules/.test(m) },
+			{ 
+				test: /\.css?$/,
 				use: [
-					"style-loader",
-					"css-loader",
+				{ loader: "style-loader" },
+				{ loader: "css-loader" },
 				]
 			},
-			{
-				test: (m) => { return /\.(png|jp(e*)g|svg)$/.test(m) },
-				exclude: (m) => { return /node_modules/.test(m) },
-				use: [{
-					loader: "url-loader",
-					options: {
-						limit: 8000,
-						name: "images/[hash]-[name].[ext]"
-					}
-				}]
-			}
 		]
 	},
 	plugins: [],
