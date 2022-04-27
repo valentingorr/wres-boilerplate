2307ab04a0bcb21c1c5d34e72cf3842bdd030a92
Valentin Gorrin <87900039+valentingorr@users.noreply.github.com>
2022-04-27 13:49:07 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require("path");
-
+ 
 module.exports = {
+	devServer: {
+		historyApiFallback: true,
+	},
 	mode: "development",
 	entry: {
 		app: [
@@ -10,23 +13,57 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				use: { loader: 'babel-loader'}
+				test: (m) => { return /\.(js|jsx)$/.test(m) },
+				exclude: (m) => { return /node_modules/.test(m) },
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: [
+							"@babel/preset-env",
+							"@babel/preset-react"
+						]
+					}
+				}
 			},
-			{ 
-				test: /\.css?$/,
+			{
+				test: (m) => { return /\.css$/.test(m) },
+				exclude: (m) => { return /node_modules/.test(m) },
 				use: [
-				{ loader: "style-loader" },
-				{ loader: "css-loader" },
+					"style-loader",
+					"css-loader"
 				]
 			},
+			{
+				test: (m) => { return /\.scss$/.test(m) },
+				exclude: (m) => { return /node_modules/.test(m) },
+				use: [
+					"style-loader",
+					"css-loader",
+					"sass-loader"
+				]
+			},
+			{
+				test: (m) => { return /\.(png|jp(e*)g)$/.test(m) },
+				exclude: (m) => { return /node_modules/.test(m) },
+				use: [{
+					loader: "url-loader",
+					options: {
+						limit: 8000,
+						name: "images/[hash]-[name].[ext]"
+					}
+				}]
+			},
+			{
+				test: (m) => { return /\.svg$/.test(m) },
+				exclude: (m) => { return /node_modules/.test(m) },
+				use: ["@svgr/webpack", "url-loader"]
+			}
 		]
 	},
 	plugins: [],
 	output: {
-	filename: "[name].bundle.js",
-	path: path.resolve(__dirname, "./dist"),
-	publicPath: "/"
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "./dist"),
+		publicPath: "/"
 	}
 };
